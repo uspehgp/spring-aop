54aeb4640e586133a5cd09ab060dc91b40a8d28a
uspehgp <z995959z>
2020-09-18 18:33:05 +0300

    27 add @annotations instead .xml

--- src/main/java/com/uspehgp/springaop/loggins/MyLogger.java
+++ src/main/java/com/uspehgp/springaop/loggins/MyLogger.java
@@ -4,11 +4,20 @@ import java.util.Map;
 import java.util.Set;
 
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Component
+@Aspect
 public class MyLogger {
 
+	@Pointcut("execution(* *(..)) && within(com.uspehgp.springaop.objects.*)")
+	private void allMethods() {
+	};
+	@Around("allMethods()")
 	public Object watchTime(ProceedingJoinPoint joinpoint) {
 		long start = System.currentTimeMillis();
 		System.out.println("method begin: " + joinpoint.getSignature().toShortString() + " >>");
@@ -30,7 +39,7 @@ public class MyLogger {
 
 		return output;
 	}
-
+	@AfterReturning(pointcut = "allMethods()", returning = "obj")
 	public void print(Object obj) {
 
 		System.out.println("Print info begin >>");
